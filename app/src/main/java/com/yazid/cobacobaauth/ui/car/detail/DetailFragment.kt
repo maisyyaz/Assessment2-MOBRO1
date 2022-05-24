99a55f43f1cdf85d80654847a7640bdd58147033
yazid138 <ma39isy@gmail.com>
2022-05-24 13:14:53 +0700

    commit

--- app/src/main/java/com/yazid/cobacobaauth/ui/car/detail/DetailFragment.kt
+++ app/src/main/java/com/yazid/cobacobaauth/ui/car/detail/DetailFragment.kt
@@ -2,11 +2,12 @@ package com.yazid.cobacobaauth.ui.car.detail
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.yazid.cobacobaauth.R
 import com.yazid.cobacobaauth.database.RoomDb
 import com.yazid.cobacobaauth.databinding.FragmentDetailBinding
@@ -25,22 +26,25 @@ class DetailFragment : Fragment() {
         val factory = CarViewModelFactory(repo)
         ViewModelProvider(this, factory)[CarViewModel::class.java]
     }
+    private val args: HomeFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val bundle = arguments
-        if (bundle != null) {
-            val args = HomeFragmentArgs.fromBundle(bundle)
-            carId = args.carId
-        }
+        carId = args.carId
         binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.getOneCar(carId).observe(viewLifecycleOwner) {
+        viewModel.getOneCar(carId).observe(requireActivity()) {
+            if (it == null) return@observe
+            val carTypeArray: Array<String> = resources.getStringArray(R.array.car_type)
+
+            binding.edtEditCarName.setText(it.name)
+            binding.edtEditCarPrice.setText(it.price.toString())
+            binding.spinnerEditCarType.setSelection(carTypeArray.indexOf(carTypeArray.first { elem -> elem == it.type }))
             Log.d("tes", "id: " + it.id + ", nama mobil: " + it.name)
         }
     }
